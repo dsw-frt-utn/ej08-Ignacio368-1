ff8b7ccb14c9bd7107ad5677ceb8023cb2ed96af
Orellana Ignacio <Ignacio.Orellana@alu.frt.utn.edu.ar>
2026-05-03 15:34:49 -0300

    Problema 6 resuelto

---
+++ Dsw2026Ej8/Problema6/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Problema6
+{
+    public static class Extensions
+    {
+        public static string ToProductCode(this string code)
+        {
+            if (code == null)
+                return "SIN-CODIGO";
+
+            string resultado = code.Trim(); // elimina espacios inicio y fin
+            resultado = resultado.ToUpper(); // mayúsculas
+            resultado = resultado.Replace(" ", "-"); // espacios → guiones
+
+            return resultado;
+        }
+    }
+}

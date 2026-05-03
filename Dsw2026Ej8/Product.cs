2a7ee1055b827f5e46af953e67f3863b80ddd4b0
Orellana Ignacio <Ignacio.Orellana@alu.frt.utn.edu.ar>
2026-05-03 14:45:25 -0300

    Problema 3 resuelto

---
+++ Dsw2026Ej8/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public class Product
+    {
+        private string _description;
+
+        public Product(string description)
+        {
+            _description = description;
+        }
+
+        public void SetDescription(string description)
+        {
+            _description = description;
+        }
+
+        public string GetDescription()
+        {
+            return _description;
+        }
+    }
+}

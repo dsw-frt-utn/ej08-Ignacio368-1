cf7f7a27f0263abb6b7d375206e1bbdc09800eaa
Orellana Ignacio <Ignacio.Orellana@alu.frt.utn.edu.ar>
2026-05-03 14:18:20 -0300

    Problema 1 resuelto

---
+++ Dsw2026Ej8/Helpers/ProductHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Helpers
+{
+    public static partial class ProductHelper
+    {
+        public static string ObtenerEtiquetaProducto(long code, string description, decimal price)
+        {
+            return $"[{code}] {description}{Separador}{price.ToString("C")}";
+        }
+    }
+}
+

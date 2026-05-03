d80d148c8d3e2b94f194ea2c4fce715bd3cb1c1e
Orellana Ignacio <Ignacio.Orellana@alu.frt.utn.edu.ar>
2026-05-03 15:23:56 -0300

    Problema 4 resuelto

---
+++ Dsw2026Ej8/Problema5/WholesaleSale.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Problema5
+{
+
+    public class WholesaleSale : Sale
+    {
+        public WholesaleSale(decimal amount) : base(amount)
+        {
+        }
+
+        public override decimal CalculateTotal()
+        {
+            return _amount * 0.9m; // 10% descuento
+        }
+    }
+}
